13cbe6cb40ffee9dd6b2f83d2fcf0c058264a6cc
Asjad Waseem <asjadwaseem@192.168.1.5>
2025-07-06 19:03:36 +0500

    feat(pr-0001): added-env-config-vars-and-sent-an-additional-key-flag-of-validity-period-to-the-be

--- src/api/services/request.ts
+++ src/api/services/request.ts
@@ -1,6 +1,16 @@
+// import axiosInstance from "@src/api/configs/axiosInstance";
+// import { FinancingFormValues } from "@src/types";
+
+// export const submitFinancingRequest = async (data: FinancingFormValues) => {
+//   return axiosInstance.post("/requests", data);
+// };
+
 import axiosInstance from "@src/api/configs/axiosInstance";
 import { FinancingFormValues } from "@src/types";
 
 export const submitFinancingRequest = async (data: FinancingFormValues) => {
-  return axiosInstance.post("/requests", data);
+  const isProd = import.meta.env.MODE === "production";
+  const endpoint = !isProd ? "/requests" : "/";
+
+  return axiosInstance.post(endpoint, data);
 };
